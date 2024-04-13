169c7c1759082dd71d051ca2ca913a9bbf0bd2b9
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-04-13 21:11:16 +0200

    interceptor using ngrx

--- src/app/Services/auth-interceptor.service.ts
+++ src/app/Services/auth-interceptor.service.ts
@@ -10,46 +10,49 @@ import { AppState } from '../app.reducer';
 import { Store } from '@ngrx/store';
 import { AuthDTO } from '../Auth/models/auth.dto';
 import { LocalStorageService } from './local-storage.service';
+import { AuthState } from '../Auth/reducers';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor(private store: Store<AppState["authApp"]>, private localStorageService: LocalStorageService) {
+  constructor(private store: Store<AppState>, private localStorageService: LocalStorageService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return this.store.select('authApp').pipe(
+      first(),
+      mergeMap((response: AuthState) => {
+        const token = response.credentials.access_token
+        if(token){
+          req = req.clone({
+            setHeaders: {
+              'Content-Type': 'application/json; charset=utf-8',
+              Accept: 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        }
+        return next.handle(req)
+      }),
+    );
   }
 
   // intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-  //   return this.store.select('credentials').pipe(
-  //     exhaustMap((credentials: AuthDTO) => {
-  //       if(credentials.access_token){
-  //         req = req.clone({
-  //           setHeaders: {
-  //             'Content-Type': 'application/json; charset=utf-8',
-  //             Accept: 'application/json',
-  //             Authorization: `Bearer ${credentials.access_token}`,
-  //           },
-  //         });
-  //       }
-  //       return next.handle(req)
-  //     }),
-  //   );
+  //   const access_token = this.localStorageService.get('access_token');
+  //   if (access_token) {
+  //     req = req.clone({
+  //       setHeaders: {
+  //         'Content-Type': 'application/json; charset=utf-8',
+  //         Accept: 'application/json',
+  //         Authorization: `Bearer ${access_token}`,
+  //       },
+  //     });
+  //   }
+
+  //   return next.handle(req);
   // }
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const access_token = this.localStorageService.get('access_token');
-    if (access_token) {
-      req = req.clone({
-        setHeaders: {
-          'Content-Type': 'application/json; charset=utf-8',
-          Accept: 'application/json',
-          Authorization: `Bearer ${access_token}`,
-        },
-      });
-    }
-
-    return next.handle(req);
-  }
 }
 
  
